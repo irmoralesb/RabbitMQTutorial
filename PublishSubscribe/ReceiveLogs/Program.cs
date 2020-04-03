4397a5df8c42ab034871ac806bc27e641dfdc248
irmoralesb <irmoralesb@hotmail.com>
2020-04-03 10:14:08 -0600

    Publish Subscriber Tutorial

---
+++ PublishSubscribe/ReceiveLogs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReveiveLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "172.17.0.2"
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                Console.Out.WriteLine(" [x] QueueName: {0}", queueName);
+                channel.QueueBind(queue: queueName,
+                    exchange: "logs",
+                    routingKey: "");
+
+                Console.Out.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.Out.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
